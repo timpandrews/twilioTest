038e79ed602e8c2ba792eca715ee00fb9e0c28b9
Tim Andrews <tim.andrews@imagineifinc.com>
2015-08-14 16:43:31 -0400

    hello tim

--- application/hellomonkey.py
+++ application/hellomonkey.py
@@ -4,10 +4,22 @@ from flask import render_template
 import twilio.twiml
 from application import app
 
+callers = {
+    "+14158675309": "Curious George",
+    "+14158675310": "Boots",
+    "+14158675311": "Virgil",
+    "+15714470280": "Tim",
+}
+
 @app.route("/", methods=['GET', 'POST'])
 def hello_monkey():
+    from_number = request.values.get('from', None)
     resp = twilio.twiml.Response()
-    resp.say("Hello Monkey")
+
+    if from_number in callers:
+        resp.say("Hello " + callers[from_number])
+    else:
+        resp.say("Hello Monkey")
 
     return str(resp)
 
