337109c37d2998eee98990e445b461c293e59978
Tim Andrews <tim.andrews@imagineifinc.com>
2015-08-17 16:38:53 -0400

    transfer call

--- application/hellomonkey.py
+++ application/hellomonkey.py
@@ -1,4 +1,4 @@
-__author__ = 'Tim'
+﻿__author__ = 'Tim'
 
 from flask import Flask, request, render_template
 import twilio.twiml
@@ -26,7 +26,29 @@ def hello_monkey():
 
     resp.play("http://demo.twilio.com/hellomonkey/monkey.mp3")
 
+    with resp.gather(numDigits=1, action="/handle-key", method="POST") as g:
+        g.say("To speak to a real monkey, press 1.  Press any other key to start over.")
+
     return str(resp)
+
+@app.route("/handle-key", methods=['GET', 'POST'])
+def handle_key():
+    """Handle key press from a user."""
+ 
+    # Get the digit pressed by the user
+    digit_pressed = request.values.get('Digits', None)
+    if digit_pressed == "1":
+        resp = twilio.twiml.Response()
+        # Dial (310) 555-1212 - connect that number to the incoming caller.
+        resp.dial("+7035730948")
+        # If the dial fails:
+        resp.say("The call failed, or the remote party hung up. Goodbye.")
+ 
+        return str(resp)
+ 
+    # If the caller pressed anything but 1, redirect them to the homepage.
+    else:
+        return redirect("/")
 
 if __name__ == "__main__":
     app.run(debug=True)
